96a7000010dd50c5d438a11595d0f7db2a9f3549
kody.low <kodylow7@gmail.com>
2023-08-16 21:50:34 -0700

    posts

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,44 @@
-use actix_web::web::get;
-use actix_web::HttpServer;
+use std::sync::Mutex;
+
+use actix_web::web::{get, post, Data};
+use actix_web::{App, HttpServer};
+
+#[derive(Clone)]
+struct Messenger {
+    message: String,
+}
+
+#[derive(Clone)]
+struct AppState {
+    messenger: Messenger,
+}
 
 #[actix_web::main]
 async fn main() {
-    let _ = HttpServer::new(|| actix_web::App::new().route("/", get().to(index)))
-        .bind(("127.0.0.1", 8000))
-        .unwrap()
-        .run()
-        .await;
+    println!("Server is running on port 8000");
+    let app_state = AppState {
+        messenger: Messenger {
+            message: "Hello".to_string(),
+        },
+    };
+
+    let _ = HttpServer::new(move || {
+        App::new()
+            .app_data(Data::new(app_state.clone()))
+            .route("/", post().to(update))
+            .route("/", get().to(index))
+    })
+    .bind(("127.0.0.1", 8000))
+    .unwrap()
+    .run()
+    .await;
 }
 
-async fn index() -> &'static str {
-    "Hello World!"
+async fn update(app_data: actix_web::web::Data<AppState>) -> String {
+    let messenger = app_data.messenger.clone();
+    format!("{} World!", messenger.message)
+}
+
+async fn index(app_data: actix_web::web::Data<AppState>) -> String {
+    app_data.messenger.message.clone()
 }
